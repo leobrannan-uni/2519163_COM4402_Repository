88379f0c535704676d7274ad64cbe159ae20ddeb
leobr <lb10crt@greatermanchester.ac.uk>
2026-01-12 09:37:36 +0000

    1.2 changed answer method to numbers 1-3 rather than abc. added more error handling to support this. debugging statements added for answer input to detect the problem.

--- Versions/2519163_COM4402_Quiz.py
+++ Versions/2519163_COM4402_Quiz.py
@@ -5,20 +5,30 @@ name = input("What is your name? ")
 print(f"\nHello, {name}. The quiz will contain 5 multiple choice questions where your answer must be either A, B, or C. Lets Begin!\n")
 
 questions = ("What German became the recent England football manager?", "What sauce is traditionally served with Turkey?", "What vehicle is used by Santa and ran using Christmas Spirit?", "The bestselling book 'Glucose Revolution' is subtitled 'the Life-changing Power of Balancing Your Blood' what?", "Meerkat Movies is a promotion by what comparison website?")
-answers = ("A. Thomas Tuchel. B. Thomas Frank. C. Thomas Muller.", "A. Mint. B. Gravy. C. Cranberry.", "A. Car. B. Sleigh. C. Bicycle.", "A. Pressure. B. Alcohol Content. C. Sugar.", "A. Compare the Market. B. Confused. C. Money Supermarket.")
-correct_answers = ("A", "C", "B", "C", "A")
+answers = ("1. Thomas Tuchel. 2. Thomas Frank. 3. Thomas Muller.", "1. Mint. 2. Gravy. 3. Cranberry.", "1. Car. 2. Sleigh. 3. Bicycle.", "1. Pressure. 2. Alcohol Content. 3. Sugar.", "1. Compare the Market. 2. Confused. 3. Money Supermarket.")
+correct_answers = (1, 3, 2, 3, 1)
 score = 0
 i = 0
 
 while i < 5:
-    print(questions[i])
+    print(f"Question {i+1}. \n{questions[i]}")
     print(answers[i])
-    choice = input("Answer: ").upper()
-    if choice == correct_answers[i]:
-        print("Correct!")
-        score = score + 1
+    choice = int(input("Answer: "))
+    if choice == int(choice):
+        if 0 < choice < 4:
+            if choice == correct_answers[i]:
+                print("Correct!")
+                score = score + 1
+            else:
+                print(f"Incorrect! the correct answer is {correct_answers[i]}")
+                #print("answer not correct")
+        else:
+            print(f"Incorrect! the correct answer is {correct_answers[i]}")
+            #print("not in range")
     else:
         print(f"Incorrect! the correct answer is {correct_answers[i]}")
+        #print("not defined as int")
+
     i = i + 1
 
 print(f"{name} your score is: {score}! Well done!")
