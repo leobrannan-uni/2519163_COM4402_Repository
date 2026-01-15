67b6418c05e636cb17fcdbf5a63407b2cf2ba67f
leobr <lb10crt@greatermanchester.ac.uk>
2026-01-15 13:03:51 +0000

    1.6 integrated PEP 8 coding standard

--- Versions/2519163_COM4402_Quiz.py
+++ Versions/2519163_COM4402_Quiz.py
@@ -9,6 +9,7 @@ def menu():
     #sleep(3)
     data(name)
 
+
 def quiz(questions, answers, correct_answers, name):
     score = 0
     i = 0
@@ -36,6 +37,7 @@ def int_range(choice, correct_answers, i):
         print(f"\nIncorrect! the correct answer is {correct_answers[i]}")
         return None
 
+
 def is_correct(choice, correct_answers, score, i):
     if choice == correct_answers[i]:
         #sleep(0.5)
@@ -46,9 +48,16 @@ def is_correct(choice, correct_answers, score, i):
     else:
         return score
 
+
 def data(name):
-    questions = ("What German became the recent England football manager?", "What sauce is traditionally served with Turkey?", "What vehicle is used by Santa and ran using Christmas Spirit?", "The bestselling book 'Glucose Revolution' is subtitled 'the Life-changing Power of Balancing Your Blood' what?", "Meerkat Movies is a promotion by what comparison website?")
-    answers = ("1. Thomas Tuchel. 2. Thomas Frank. 3. Thomas Muller.", "1. Mint. 2. Gravy. 3. Cranberry.", "1. Car. 2. Sleigh. 3. Bicycle.", "1. Pressure. 2. Alcohol Content. 3. Sugar.", "1. Compare the Market. 2. Confused. 3. Money Supermarket.")
+    questions = ("What German became the recent England football manager?",
+                 "What sauce is traditionally served with Turkey?",
+                 "What vehicle is used by Santa and ran using Christmas Spirit?",
+                 "The bestselling book 'Glucose Revolution' is subtitled 'the Life-changing Power of "
+                 "Balancing Your Blood' what?", "Meerkat Movies is a promotion by what comparison website?")
+    answers = ("1. Thomas Tuchel. 2. Thomas Frank. 3. Thomas Muller.", "1. Mint. 2. Gravy. 3. Cranberry.",
+               "1. Car. 2. Sleigh. 3. Bicycle.", "1. Pressure. 2. Alcohol Content. 3. Sugar.",
+               "1. Compare the Market. 2. Confused. 3. Money Supermarket.")
     correct_answers = (1, 3, 2, 3, 1)
     quiz(questions, answers, correct_answers, name)
 
