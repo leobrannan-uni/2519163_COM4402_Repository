9f005c80cf385efce8e688eb205d9207f40f57b2
leobr <lb10crt@greatermanchester.ac.uk>
2026-01-19 15:23:50 +0000

    2.0 error handling has been perfected, with some function name changes. PEP8 coding conventions added (commenting in next version)

--- Versions/2519163_COM4402_Quiz.py
+++ Versions/2519163_COM4402_Quiz.py
@@ -7,7 +7,7 @@ def menu():
     print("Welcome to the Quiz!!")
     name = input("\nWhat is your name? ")
     print(f"\nHello, {name}. The quiz will contain 5 multiple choice questions.")
-    print("Your answer must be either 1, 2, or 3. Lets Begin!")
+    print("Your answer must be either 1, 2, or 3. Lets Begin!\n")
     sleep(3)
     data(name)
 
@@ -21,29 +21,38 @@ def quiz(name, df):
     score = 0
     i = 0
     while i < 5:
-        print("\n\n#####################\n")
-        print(f"Question {i + 1}.\n")
-        print(df.iloc[i]["Questions"])
-        sleep(1)
-        print(df.iloc[i]["Answers"])
-        choice = input("\nAnswer: ")
+        question(df, i)
         sleep(0.5)
-        choice = int_range(choice)
+        choice = choice_range(df, i)
         score = is_correct(choice, df.iloc[i]["Correct Answers"], score)
         i = i + 1
-        sleep(2)
+        sleep(1.5)
     print (f"\n\n{name} your final score is: {score}/5! Well done!")
 
 
-def int_range(choice):
-    try:
-        choice = int(choice)
-        if 0 < choice < 4:
-            return int(choice)
-        else:
-            return choice
-    except ValueError:
-        return choice
+def question(df, i):
+    print("\n#####################\n")
+    print(f"Question {i + 1}.\n")
+    print(df.iloc[i]["Questions"])
+    sleep(1)
+    print("\n", df.iloc[i]["Answers"])
+
+
+def choice_range(df, i):
+    while True:
+        choice = input("\nAnswer: ")
+        try:
+            choice = int(choice)
+            if 0 < choice < 4:
+                return int(choice)
+            else:
+                print("Enter a number between 1 and 3. Please try again.")
+                sleep(1)
+                question(df, i)
+        except ValueError:
+            print("Incorrect value entered, please try again.")
+            sleep(1)
+            question(df, i)
 
 
 def is_correct(choice, correct_answers, score):
