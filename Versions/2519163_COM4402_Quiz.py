06582a2dc2f0fe6957b9896f3688d53481020dc8
leobr <lb10crt@greatermanchester.ac.uk>
2026-01-19 14:27:01 +0000

    1.9 data has been converted to a csv file and code has been adjusted to handle these changes. Functions are now in chronological order

--- Versions/2519163_COM4402_Quiz.py
+++ Versions/2519163_COM4402_Quiz.py
@@ -1,27 +1,35 @@
 from time import sleep
+import pandas as pd
 
 
 def menu():
     print("\n\n#####################\n")
     print("Welcome to the Quiz!!")
     name = input("\nWhat is your name? ")
-    print(f"\nHello, {name}. The quiz will contain 5 multiple choice questions where your answer must be either 1, 2, or 3. Lets Begin!")
+    print(f"\nHello, {name}. The quiz will contain 5 multiple choice questions.")
+    print("Your answer must be either 1, 2, or 3. Lets Begin!")
     sleep(3)
     data(name)
 
 
-def quiz(questions, answers, correct_answers, name):
+def data(name):
+    df = pd.read_csv("data.csv")
+    quiz(name, df)
+
+
+def quiz(name, df):
     score = 0
     i = 0
     while i < 5:
         print("\n\n#####################\n")
-        print(f"Question {i + 1}. \n{questions[i]}\n")
+        print(f"Question {i + 1}.\n")
+        print(df.iloc[i]["Questions"])
         sleep(1)
-        print(answers[i])
+        print(df.iloc[i]["Answers"])
         choice = input("\nAnswer: ")
         sleep(0.5)
         choice = int_range(choice)
-        score = is_correct(choice, correct_answers, score, i)
+        score = is_correct(choice, df.iloc[i]["Correct Answers"], score)
         i = i + 1
         sleep(2)
     print (f"\n\n{name} your final score is: {score}/5! Well done!")
@@ -38,29 +46,14 @@ def int_range(choice):
         return choice
 
 
-def is_correct(choice, correct_answers, score, i):
-    if choice == correct_answers[i]:
+def is_correct(choice, correct_answers, score):
+    if choice == correct_answers:
         print("\nCorrect!")
         score = score + 1
         return score
     else:
-        print(f"\nIncorrect! the correct answer is {correct_answers[i]}")
+        print(f"\nIncorrect! the correct answer is {correct_answers}")
         return score
-
-
-def data(name):
-    questions = ("What German became the recent England football manager?",
-                 "What sauce is traditionally served with Turkey?",
-                 "What vehicle is used by Santa and ran using Christmas Spirit?",
-                 "The bestselling book 'Glucose Revolution' is subtitled 'the "
-                 "Life-changing Power of Balancing Your Blood' what?",
-                 "Meerkat Movies is a promotion by what comparison website?")
-    answers = ("1. Thomas Tuchel. 2. Thomas Frank. 3. Thomas Muller.",
-               "1. Mint. 2. Gravy. 3. Cranberry.", "1. Car. 2. Sleigh. 3. Bicycle.",
-               "1. Pressure. 2. Alcohol Content. 3. Sugar.",
-               "1. Compare the Market. 2. Confused. 3. Money Supermarket.")
-    correct_answers = (1, 3, 2, 3, 1)
-    quiz(questions, answers, correct_answers, name)
 
 
 if __name__ == "__main__":
